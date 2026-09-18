6707f4f26f7ea50ff0d0227e14df7c4063ac46ba
Elsazly161107 <a.w.elshazly.business@gmail.com>
2026-09-18 16:12:15 +0300

    js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function toggleTable() {
 
 // Msg hidden
 
-let sectionInP = document.querySelector("section form");
+// let sectionInP = document.querySelector("section form");
 
 // sectionIn.addEventListener("click", function () {
 //   sectionInP.classList.add("open");
@@ -69,7 +69,7 @@ function tdOpacity() {
 
 // --- حدث الضغط على زر البحث ---
 searchBtn.addEventListener("click", () => {
-  sectionInP.classList.remove("open");
+  // sectionInP.classList.remove("open");
   toggleTable();
   filterData(data); // 'data' موجودة في مكان آخر
   populateTable();
